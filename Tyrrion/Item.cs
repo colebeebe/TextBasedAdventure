2536b8ba47a60fc101a8b9f0b4e5acb2ae514b39
colebeebe <bee14003@byui.edu>
2025-10-14 18:33:40 -0400

    Fixed item getters and setters

--- Tyrrion/Item.cs
+++ Tyrrion/Item.cs
@@ -6,8 +6,8 @@ public class Item {
     public string Description { get; set; } = "";
     public int Capacity { get; set; } = 0;
     public int Size { get; set; } = 1;
-    public List<string> Alternatives = [];
-    public Dictionary<string, string> Actions = [];
-    public List<string> Items = [];
+    public List<string> Alternatives { get; set; } = [];
+    public Dictionary<string, string> Actions { get; set; } = [];
+    public List<string> Items { get; set; } = [];
 }
 
